34d0792f131f8507248fdac9e3ded4e757d1ea3e
Filip Kopecky <kopecfil@gmail.com>
2025-11-02 16:26:07 +0100

    Project created, compiles, runs

---
+++ WebApp/Controllers/PingController.cs
@@ -0,0 +1,7 @@
+﻿using System.Web.Http;
+
+public sealed class PingController : ApiController
+{
+	[HttpGet, Route("api/ping")]
+	public IHttpActionResult Get() => Ok(new { ok = true, framework = ".NET Framework Web API 2" });
+}
